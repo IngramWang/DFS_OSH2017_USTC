7a22e33944cdee2cd03688c33eb9b29e4094e03d
wyr <cnwj@mail.ustc.edu.cn>
2017-05-07 17:50:41 +0800

    rebulid server's code

---
+++ JunWang_Server/server/DFS_server.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class DFS_server {
+	
+	public static void main(String args[]) throws Exception{
+     	int port = 6661;
+      	try{
+	        Thread t = new ServerThread(port);
+	        t.start();
+      	}catch(IOException e){
+      		e.printStackTrace();
+      	}   	 	
+	}
+}
---
+++ JunWang_Server/server/ServerThread.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class ServerThread extends Thread {
+		ServerSocket server;
+		public ServerThread(int port) throws IOException{
+			server = new ServerSocket(port);
+			//server.setSoTimeout(10000);
+		}
+		public void run(){
+			while(true){
+				try {
+					Socket socket = server.accept();
+		//			socket.setSoTimeout(10000);
+					ClientThread thread =  new ClientThread(socket);
+					thread.start();
+					    /*{catch(SocketTimeoutException s){
+            			System.out.println("Socket timed out!");
+                    }*/
+                    System.out.println("accepted0!\n");
+                    }catch (IOException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+					}
+			}
+		}
+	}	
