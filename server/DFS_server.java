23fad5f6aebbd35dd0c2722512ad6b1e4c595aff
wyr <cnwj@mail.ustc.edu.cn>
2017-05-10 18:42:18 +0800

    modified server and datebase

--- server/DFS_server.java
+++ server/DFS_server.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class DFS_server {
 	
 	public static void main(String args[]) throws Exception{
-     	int port = 6661;
+     	int port = 6668;
       	try{
 	        Thread t = new ServerThread(port);
 	        t.start();
